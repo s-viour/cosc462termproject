3715efff8c1e6ff5bf309d512ef18cdb2b527fcf
matt wyatt <mwyatt1000@gmail.com>
2022-12-01 20:57:52 -0500

    add comments to timer.h

--- include/timer.h
+++ include/timer.h
@@ -11,10 +11,23 @@ private:
 public:
   timer() : _compute_time(0), _communication_time(0) {}
 
+  /// @brief begin counting computation time
   void start_compute();
+
+  /// @brief begin counting communication time
   void start_communication();
+
+  /// @brief stop counting computation time (for now)
   void end_compute();
+
+  /// @brief stop counting communication time (for now)
   void end_communication();
+
+  /// @brief get the total compute time counted so far
+  /// @return compute time in seconds
   double total_compute_time();
+
+  /// @brief get the total communication time counted so far
+  /// @return communication time in seconds
   double total_communication_time();  
 };
